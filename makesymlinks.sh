ddddd694be7bc6ab1456ccb8eee9ea719fb509e2
zwergius <webmaster@zwergius.com>
2024-01-12 17:15:56 +0100

    Adds alacritty config and remove lvim copy

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,10 +8,12 @@
 
 dir=~/Code/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
+configdir=~/.config
 lvimconfigdir=~/.config/lvim
 files="brewfile vim_pluginsrc aliases bashrc zshrc gitconfig bash_profile vimrc vim gitignore"    # list of files/folders to symlink in homedir
 lvimfiles="config.lua vimrc-auto-mkdir.vim"
 configfolder="lvim"
+configfolders="alacritty"
 
 ##########
 
@@ -33,20 +35,20 @@ for file in $files; do
     ln -f -s $dir/$file ~/.$file
 done
 
-for file in $lvimfiles; do
-  echo "Moving existing config files ($lvimfiles) from $lvimconfigdir to $olddir"
-  mv $lvimconfigdir/$file $olddir
-  echo "Creating symlink to $file in $lvimconfigdir"
-  ln -f -s $dir/$configfolder/$file $lvimconfigdir/$file
-done
-
-# move any existing folder in .config to dotfiles_old, then create symlinks
-# for folder in $configfolders; do
-#   echo "Backing up $folder in $configdir to .dotfiles_old"
-#   mv $configdir/$folder $olddir
-#   echo "Creating symlink to $folder in home $configdir directory."
-#   ln -f -s $dir/$folder $configdir/$folder
+# for file in $lvimfiles; do
+#   echo "Moving existing config files ($lvimfiles) from $lvimconfigdir to $olddir"
+#   mv $lvimconfigdir/$file $olddir
+#   echo "Creating symlink to $file in $lvimconfigdir"
+#   ln -f -s $dir/$configfolder/$file $lvimconfigdir/$file
 # done
+
+#move any existing folder in .config to dotfiles_old, then create symlinks
+for folder in $configfolders; do
+  echo "Backing up $folder in $configdir to .dotfiles_old"
+  mv $configdir/$folder $olddir
+  echo "Creating symlink to $folder in home $configdir directory."
+  ln -f -s $dir/$folder $configdir/$folder
+done
 
 # installs antibody if needed
 #S$dir/antibody/install.sh
